3174228ee41f5dc03ab446d6f5a3ec05ca9432bc
Paul Watson <watsonbpaul80@gmail.com>
2024-09-11 10:14:51 +0100

    added env file

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 
 :START
@@ -25,38 +24,3 @@ if "%choice%"=="1" (
 
 REM Pause to see any error messages
 pause
-=======
-@echo off
-
-:START
-REM Prompt the user to choose between tracker, collect, or test
-echo Choose an option:
-echo 1. Run Tracker
-echo 2. Run Collect 
-echo 3. Royal Ascot
-echo 4. Tracker login
-
-set /p choice=Enter option number:
-
-REM Check the user's choice and execute the corresponding script
-if "%choice%"=="1" (
-    REM Call tracker.js
-    node tracker.js
-) else if "%choice%"=="2" (
-    REM Call collect.js
-    node collect.js
-) else if "%choice%"=="3" (
-    REM Call royalascot.js
-    node royalascot.js
-) else if "%choice%"=="4" (
-    REM Call trackerlogin.js
-    node trackerlogin.js	
-) else (
-    REM If the input is invalid, display an error message
-    echo Invalid option selected. Please enter either 1, 2, 3, 4, or 5.
-    goto START
-)
-
-REM Pause to see any error messages
-pause
->>>>>>> 72a64cd (Initial commit)
